7c2699d38ba383d9d137a47286d1c8eb035ca1b1
abirembaut <anthony.birembaut@gmail.com>
2023-09-19 01:41:17 +0200

    feat(REST error codes handling): Handle 401 and 503 error on Bonita REST requests
    * Add interceptor to reload the top level Bonita app page when there is a 401 or a 503 and the request if for Bonita REST API
    
    Covers [DEV-485](https://bonitasoft.atlassian.net/browse/DEV-485)
    Relates to [RUNTIME-531](https://bonitasoft.atlassian.net/browse/RUNTIME-531)

--- generator-angularjs/src/main/runtime/js/runtime.config.js
+++ generator-angularjs/src/main/runtime/js/runtime.config.js
@@ -4,6 +4,8 @@
   angular.module('bonitasoft.ui.services').config($httpProvider => {
     // configure bonita XSRF token cookie and header names
     $httpProvider.defaults.xsrfHeaderName = $httpProvider.defaults.xsrfCookieName = 'X-Bonita-API-Token';
+    // configure interceptor to handle errors when calling Bonita REST APIs
+    $httpProvider.interceptors.push('restInterceptorFactory');
   });
 })();
 
