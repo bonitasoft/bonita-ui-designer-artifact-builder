e3d6a00e38d03e5a21043600e05b0b7574bc78d8
abirembaut <anthony.birembaut@gmail.com>
2023-10-11 23:33:27 +0200

    fix(model version): Revert model version as it had already been updated for 1.17.1

--- artifact-builder/src/main/java/org/bonitasoft/web/designer/UiDesignerCoreFactory.java
+++ artifact-builder/src/main/java/org/bonitasoft/web/designer/UiDesignerCoreFactory.java
@@ -128,8 +128,7 @@ public class UiDesignerCoreFactory {
                         new BusinessVariableMigrationStep<>()),
                 new Migration<>("2.3", new SetInterpretHtmlTrueMigrationStep<>(componentVisitor)),
                 new Migration<>("2.4", new AccessibilityCheckListAndRadioButtonsMigrationStep(componentVisitor)),
-                new Migration<>("2.5", new AddModelVersionMigrationStep<>("2.5")),
-                new Migration<>("2.6", new AddModelVersionMigrationStep<>("2.6")));
+                new Migration<>("2.5", new AddModelVersionMigrationStep<>("2.5")));
 
         List<Migration<Page>> pageMigrationStepsList = List.<Migration<Page>> of(
                 new Migration<>("1.0.2", new AssetIdMigrationStep<>()),
@@ -158,8 +157,8 @@ public class UiDesignerCoreFactory {
                 new Migration<>("2.2", new AddModelVersionMigrationStep<>("2.2")),
                 new Migration<>("2.3", new SetInterpretHtmlTrueMigrationStep<>(componentVisitor)),
                 new Migration<>("2.4", new AccessibilityCheckListAndRadioButtonsMigrationStep(componentVisitor)),
-                new Migration<>("2.5", new StyleUpdateInputTypeMigrationStep(pageAssetService)),
-                new Migration<>("2.6", new StyleAddErrorDialogPropertiesMigrationStep(pageAssetService),
+                new Migration<>("2.5", new StyleUpdateInputTypeMigrationStep(pageAssetService),
+                        new StyleAddErrorDialogPropertiesMigrationStep(pageAssetService),
                         new LocalizationAddErrorDialogTranslationsMigrationStep(pageAssetService)));
 
         List<Migration<Widget>> widgetMigrationStepsList = List.<Migration<Widget>> of(
@@ -171,8 +170,7 @@ public class UiDesignerCoreFactory {
                 new Migration<>("2.2", new AddModelVersionMigrationStep<>("2.2")),
                 new Migration<>("2.3", new AddModelVersionMigrationStep<>("2.3")),
                 new Migration<>("2.4", new AddModelVersionMigrationStep<>("2.4"), new AddWebResourcesForWidget()),
-                new Migration<>("2.5", new AddModelVersionMigrationStep<>("2.5")),
-                new Migration<>("2.6", new AddModelVersionMigrationStep<>("2.6")));
+                new Migration<>("2.5", new AddModelVersionMigrationStep<>("2.5")));
 
         var widgetMigrationApplyer = new WidgetMigrationApplyer(widgetMigrationStepsList);
         var widgetIdVisitor = new WidgetIdVisitor(fragmentRepository);
